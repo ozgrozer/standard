40381dffcda7ffddc13b8bf5a7f5ae9a30ed21c6
mac <ozgr@live.com>
2024-05-19 02:52:56 +0300

    Add next boilerplate

---
+++ pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Standard Fix</title>
+      </Head>
+
+      <div>Hello World!</div>
+    </>
+  )
+}
