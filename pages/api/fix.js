50718ba1ea75c61905195f313c9525ef180e484a
mac <ozgr@live.com>
2024-05-23 22:46:08 +0300

    Use prettier

--- pages/api/fix.js
+++ pages/api/fix.js
@@ -1,25 +1,42 @@
-import { ESLint } from 'eslint'
+import prettier from 'prettier'
+import jscodeshift from 'jscodeshift'
+import configStandard from 'prettier-config-standard'
 
-const eslint = new ESLint({
-  fix: true,
-  useEslintrc: false,
-  overrideConfig: {
-    extends: ['standard']
-  }
-})
-
-const fixCode = async code => {
+const transformToES6 = code => {
   try {
-    const results = await eslint.lintText(code)
-    const fixedCode = results[0].output
-    return fixedCode
+    const j = jscodeshift.withParser('babel')
+    const root = j(code)
+
+    root.find(j.FunctionDeclaration).replaceWith(path => {
+      const { node } = path
+      const arrowFunction = j.arrowFunctionExpression(node.params, node.body, node.async)
+      return j.variableDeclaration('const', [
+        j.variableDeclarator(j.identifier(node.id.name), arrowFunction)
+      ])
+    })
+
+    root.find(j.FunctionExpression).replaceWith(path => {
+      const { node } = path
+      return j.arrowFunctionExpression(node.params, node.body, node.async)
+    })
+
+    return root.toSource()
   } catch (err) {
-    console.log(err.message)
+    throw new Error(err.message)
   }
 }
 
 export default async (req, res) => {
-  const code = req.body.code
-  const fixedCode = await fixCode(code)
-  res.json({ success: true, fixedCode })
+  let fixedCode = ''
+  try {
+    const transformedCode = transformToES6(req.body.code)
+
+    fixedCode = await prettier.format(transformedCode, {
+      ...configStandard,
+      parser: 'babel'
+    })
+  } catch (err) {
+    fixedCode = err.message
+  }
+  res.json({ fixedCode })
 }
